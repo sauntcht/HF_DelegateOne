f693cd17bd35f79a95788bbb2d1c7fd9573b0789
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-05-02 15:15:21 -0400

    Add project files.

---
+++ HF_DelegateOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HF_DelegateOne
+{
+    internal class Program
+    {
+        delegate string IntToString(int i);
+
+        public static string AddNumberSign(int i) => $"#{i}";
+        public static string PlusOne(int i) => $"{i} plus one equals {i + 1}";
+
+        static void Main(string[] args)
+        {
+            IntToString methodRef = AddNumberSign;
+            Console.WriteLine(methodRef(3212));
+
+            methodRef = PlusOne;
+            Console.WriteLine(methodRef(32));
+        }
+    }
+}
